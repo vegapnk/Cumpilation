240fcf94f841151284f254f45ca6f7a7fbe70e7a
Vegapnk <vegapnk@protonmail.com>
2024-09-03 18:35:05 +0200

    Updated storing fluids

--- 1.5/Source/Common/HediffComps/HediffComp_FluidChange.cs
+++ 1.5/Source/Common/HediffComps/HediffComp_FluidChange.cs
@@ -1,4 +1,5 @@
 ﻿using Cumpilation.Cumflation;
+using RimWorld;
 using rjw;
 using System;
 using System.Collections.Generic;
@@ -15,7 +16,10 @@ namespace Cumpilation.Common
         public HediffCompProperties_FluidChange Props => (HediffCompProperties_FluidChange)this.props;
 
 
-        List<Hediff> changedParts = new List<Hediff>();
+        //Dictionary<Hediff,SexFluidDef> changedParts = new Dictionary<Hediff,SexFluidDef>();
+
+        List<Hediff> storedHediffs = new List<Hediff>();
+        List<SexFluidDef> storedDefs = new List<SexFluidDef>();
 
         public override void CompPostPostAdd(DamageInfo? dinfo)
         {
@@ -30,39 +34,43 @@ namespace Cumpilation.Common
                 {
                     if (Props.changePenis && Genital_Helper.is_penis(part) && sexPart.GetPartComp().Fluid != Props.fluid)
                     {
-                        sexPart.GetPartComp().Fluid = Props.fluid; 
-                        changedParts.Add(part);
+                        StoreAndChangeFluid(part);
                     }
                     else if (Props.changeVagina && Genital_Helper.is_vagina(part) && sexPart.GetPartComp().Fluid != Props.fluid)
                     {
-                        sexPart.GetPartComp().Fluid = Props.fluid;
-                        changedParts.Add(part);
+                        StoreAndChangeFluid(part);
                     }
                     else if (Props.changeBreast && (part.def.defName.ToLower().Contains("breast") || part.def.defName.ToLower().Contains("udder")) && sexPart.GetPartComp().Fluid != Props.fluid)
-                    {
-                        sexPart.GetPartComp().Fluid = Props.fluid;
-                        changedParts.Add(part);
-                    }
+                        StoreAndChangeFluid(part);
                     else if (Props.changeOther && sexPart.GetPartComp().Fluid != Props.fluid)
                     {
-                        sexPart.GetPartComp().Fluid = Props.fluid;
-                        changedParts.Add(part);
+                        StoreAndChangeFluid(part);
                     }
                 }
             }
-            ModLog.Debug($"Changed {changedParts.Count()} fluids for {this.parent.pawn} to {Props.fluid}");
+            ModLog.Debug($"Changed {storedHediffs.Count()} fluids for {this.parent.pawn} to {Props.fluid}");
         }
 
+
+        protected void StoreAndChangeFluid(Hediff part)
+        {
+            // DevNote: Due to the logic before, I assume all of these Casts are save and have values;
+            ISexPartHediff sexPart = (ISexPartHediff)part;
+            var comp = sexPart.GetPartComp();
+            storedHediffs.Add(part);
+            storedDefs.Add(comp.Fluid);
+            comp.Fluid = Props.fluid;
+        }
 
         public override void CompPostPostRemoved()
         {
             base.CompPostPostRemoved();
-            ModLog.Debug($"Resetting Fluids for {this.parent.pawn} of {changedParts.Count()} SexParts");
-            foreach (Hediff part in changedParts)
-            {
+            ModLog.Debug($"Resetting Fluids for {this.parent.pawn} of {storedDefs.Count()} SexParts");
+            for (int i = 0; i < storedHediffs.Count; i++) { 
+                Hediff part = storedHediffs[i];
                 if (part is ISexPartHediff sexPart)
                 {
-                    sexPart.GetPartComp().Fluid = sexPart.GetPartComp().Def.fluid;
+                    sexPart.GetPartComp().Fluid = storedDefs[i];
                 }
             }
         }
@@ -70,10 +78,25 @@ namespace Cumpilation.Common
         public override void CompExposeData()
         {
             base.CompExposeData();
-            Scribe_Collections.Look<Hediff>(ref changedParts, "changedParts", lookMode:LookMode.Reference);
+
+            Scribe_Collections.Look<Hediff>(ref storedHediffs, "storedHediffs", LookMode.Reference);
+            Scribe_Collections.Look<SexFluidDef>(ref storedDefs, "storedDefs", LookMode.Def);
+            /*
+             * DevNote: Originally I had a Dictionary, but this was giving me a big big headache with the Scribes. 
+             * Someone from the Rimworld Discord tried to help me but I kept getting errors and weird values. 
+             * So for now it is two lists, because why not actually. Two lists work quite reliably.
+            var workingKeys = new List<Hediff>();
+            var workingValues = new List<SexFluidDef>();
+
+            Scribe_Collections.Look<Hediff,SexFluidDef>(
+                ref changedParts, "changedParts", keyLookMode:LookMode.Reference, valueLookMode:LookMode.Def,
+                keysWorkingList: ref workingKeys, valuesWorkingList: ref workingValues
+                ,log
+                );
+            */
         }
 
 
-        public bool WasSuccessfullyApplied() => changedParts.Any();
+        public bool WasSuccessfullyApplied() => storedHediffs.Any();
     }
 }
