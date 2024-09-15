45ed05a2c227b16a03acfdad05272dee62ae1f47
Vegapnk <vegapnk@protonmail.com>
2024-09-15 11:13:09 +0200

    Start on BlueBalling and Draining

---
+++ 1.5/Source/Common/HediffComps/HediffComp_ChangeFluidfactorBasedOnSeverity.cs
@@ -0,0 +1,63 @@
+﻿using rjw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Cumpilation.Common
+{
+    /// <summary>
+    /// Adjusts the targetted SexParts fluid-amount based on a linear factor between a configured Max and Min value. 
+    /// 
+    /// Example: Max 5, Min 2
+    /// Severity 100% -> 5.0
+    /// Severity 50%  -> 3.5
+    /// Severity 0%   -> 2.0
+    /// Severity 33%  -> 3.0
+    /// Severity 66%  -> 4.0
+    /// Severity 250% -> 5.0
+    /// </summary>
+    public class HediffComp_ChangeFluidfactorBasedOnSeverity : HediffComp
+    {
+
+        protected float lastMultiplier = float.MinValue;
+
+        public new HediffCompProperties_ChangeFluidfactorBasedOnSeverity Props => (HediffCompProperties_ChangeFluidfactorBasedOnSeverity)this.props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (severityAdjustment != 0) {
+                foreach (ISexPartHediff sexPart in Props.GetSexPartHediffs(parent.pawn)) {
+
+                    if ( (lastMultiplier != float.MinValue) && lastMultiplier != 0) 
+                    {
+                        sexPart.GetPartComp().partFluidFactor /= lastMultiplier;
+                    }
+                    sexPart.GetPartComp().partFluidFactor *= CalculateOutput();
+                    lastMultiplier = CalculateOutput();
+                }
+            }
+        }
+
+        protected float CalculateOutput()
+        {
+            float sev = parent.Severity;
+
+            // Calculate the value at the given percentage between min and max
+            float result = Props.min + (sev) * (Props.max - Props.min);
+            return result;
+        }
+
+        public override void CompExposeData()
+        {
+            base.CompExposeData();
+
+            Scribe_Values.Look(ref lastMultiplier, "lastMultiplier", 1.0f);
+        }
+
+    }
+}
---
+++ 1.5/Source/Fluids/Cum/Patch_HediffSexPart_Tick_GetBlueBalls.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using rjw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Cumpilation.Fluids.Cum
+{
+
+    [HarmonyPatch(typeof(Hediff), nameof(Hediff.PostTick))]
+    public static class Patch_HediffSexPart_Tick_GetBlueBalls
+    {
+        public static void Postfix(Hediff __instance)
+        {
+            if (__instance != null && __instance is ISexPartHediff sexPart)
+            {
+                Pawn pawn = __instance.pawn;
+                if (pawn == null || pawn.IsAnimal() || pawn.Dead) return;
+
+                // Do only touch things on this Map and spawned --- helps to avoid ticking up hediffs in pawns outside of view.
+                if (!pawn.Spawned || !pawn.Map.IsPlayerHome) return;
+
+                if (sexPart.GetPartComp().Def == null || sexPart.GetPartComp().Def.fluid == null) 
+                    return;
+                
+                // Requirement: The fluid must be "storable", defined as a tag. 
+                // DevNote: I wanted this to be at the Genital, but 
+                if (!sexPart.GetPartComp().Def.fluid.tags.Contains("CanBlueBall"))
+                    return; 
+
+                if (__instance.pawn != null && __instance.pawn.IsHashIntervalTick(1000))
+                {
+                    ModLog.Debug($"Postfixing Patch_HediffSexPart_Tick_GetBlueBalls for {__instance} of {__instance.pawn}");
+                    // TODO: Create or Find hediff & Bump Hediff Severity 
+                }
+            }
+        }
+
+    }
+}
