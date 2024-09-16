abcd932ba321e04077e59dcd0e6cf327c7f9420b
Vegapnk <vegapnk@protonmail.com>
2024-09-16 13:18:49 +0200

    Blue-Balls and Wet-Girls can spawn filth at high stages

---
+++ 1.5/Source/Common/HediffComps/HediffCompProperties_RandomlySpawnFilth.cs
@@ -0,0 +1,28 @@
+﻿using Cumpilation.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cumpilation.Common
+{
+    public class HediffCompProperties_RandomlySpawnFilth : HediffCompProperties_PartTargetting
+    {
+        public int ticksBetweenCheck;
+        public float chanceToSpawn;
+
+        /// <summary>
+        /// Scale the chance based on severity. If Chance is 50% and Severity is 50%, then there is a 25% final chance.
+        /// </summary>
+        public bool chanceBasedOnSeverity = false;
+        /// <summary>
+        /// Only try to spawn filth if the current stage is higher than the specified number.
+        /// </summary>
+        public int onlyAtStagesHigherThan = int.MinValue;
+
+
+        public HediffCompProperties_RandomlySpawnFilth() => this.compClass = typeof(HediffComp_RandomlySpawnFilth);
+
+    }
+}
