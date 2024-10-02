cc1ea63dd122c648c06a0e781436ac20fde0772c
Vegapnk <vegapnk@protonmail.com>
2024-10-02 17:49:59 +0200

    Added HediffComp Property for Spawning another Hediff on Interval

---
+++ 1.5/Source/Common/HediffComps/HediffCompProperties_SpawnOtherHediffOverTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Cumpilation.Common
+{
+    public class HediffCompProperties_SpawnOtherHediffOverTime : HediffCompProperties_PawnTargetting
+    {
+
+        public int tickInterval;
+        public float severityIncrease;
+        public HediffDef hediff;
+
+        public bool TryToSpawnInSameBodyPart = true;
+        public float applicationChance = 1.0f;
+
+    }
+}
---
+++ 1.5/Source/Common/HediffComps/HediffComp_SpawnOtherHediffOverTime.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Cumpilation.Common
+{
+    public class HediffComp_SpawnOtherHediffOverTime : HediffComp
+    {
+        public HediffCompProperties_SpawnOtherHediffOverTime Props => (HediffCompProperties_SpawnOtherHediffOverTime)this.props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (this.props != null && Pawn.IsHashIntervalTick(Props.tickInterval) && Props.IsValidPawn(Pawn)) { 
+                if ((new Random()).NextDouble() <= Props.applicationChance){
+
+                    Hediff spawnedHediff = Pawn.health.hediffSet.GetFirstHediffOfDef(Props.hediff);
+                    if (spawnedHediff == null)
+                    {
+                        BodyPartRecord bpr = Props.TryToSpawnInSameBodyPart ? parent.Part : null;
+                        Hediff hediff = HediffMaker.MakeHediff(Props.hediff, Pawn, bpr);
+                    }
+                    spawnedHediff.Severity += Props.severityIncrease;
+                }
+            }
+        }
+    }
+}
